9d01246ff4704379caa119227db15ecdb01d3309
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-04-20 18:25:36 +0900

    #S06P31A107-61 Feat: Resizing 로직 구현

--- frontend/component/memo/memoCommon/MemoFrame.tsx
+++ frontend/component/memo/memoCommon/MemoFrame.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import React, { useEffect, useState } from 'react'
+import { Resizable } from 're-resizable'
+import type { NextPage } from 'next'
 
-import type { NextPage } from "next";
-
-interface Props{
-    width: number;
-    height: number;
-    header?: string;
-    content?: any;
+interface Props {
+  width: number
+  height: number
+  header?: string
+  content?: any
+}
+const style = {
+  position: 'relative',
+  border: 'solid 1px #ddd',
+  background: '#f0f0f0',
+  borderRadius: '15px',
+} as const
+const headerStyle = {
+  verticalAlign: 'top',
+  alignItems: 'left',
+} as const
+const contentStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+} as const
+const MemoFrame: NextPage<Props> = ({ width, height, content, header }) => {
+  const [size, setSize] = useState({
+    width: width,
+    height: height,
+  })
+  useEffect(() => {
+    console.log(size)
+  }, [size])
+  return (
+    <Resizable
+      style={style}
+      size={{
+        width: size.width,
+        height: size.height,
+      }}
+      onResizeStop={(e, direction, ref, d) => {
+        setSize({ width: size.width + d.width, height: size.height + d.height })
+      }}
+    >
+      <div style={headerStyle}>{header}</div>
+      <div style={contentStyle}> {content}</div>
+    </Resizable>
+  )
 }
 
-const MemoFrame: NextPage<Props> = ({width, height, content, header}) => {
-    return (
-            <Card sx={{ width: width, height: height}}>
-                <CardContent>
-                    <Typography variant="h5" component="div">
-                        {header}
-                    </Typography>{content}</CardContent>
-            </Card>
-    );
-};
-
-export default MemoFrame;
+export default MemoFrame
