b5e4d0dffa2a6029afb314e35f1d8a5d89f512e4
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-04-26 15:29:25 +0900

    #S06P31A107-103 Feat: 체크리스트 기능 구현

--- frontend/component/memo/memoCheckList/MemoChecklist.tsx
+++ frontend/component/memo/memoCheckList/MemoChecklist.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MemoFrame from '../memoCommon/MemoFrame';
 
 const memoChecklist = () => {
+    const [checkboxInfo, setCheckboxInfo] = useState([{
+        content: 'test',
+        isChecked: false
+    },{
+        content: 'test2',
+        isChecked: true
+    }])
+    
+    const onCheckboxClick = (index) =>{
+        checkboxInfo[index].isChecked = !checkboxInfo[index].isChecked
+        setCheckboxInfo([...checkboxInfo])
+    }
+
+    const addCheckboxList = () =>{
+        setCheckboxInfo([...checkboxInfo, {content: "추가하기 test", isChecked: false}])
+    }
+
     return (
         <div>
-            
+            {checkboxInfo.length > 0 && checkboxInfo.map((checkbox, index)=>{
+                return(
+                    <div>
+                        <input type='checkbox' checked={checkbox.isChecked} onClick={()=>onCheckboxClick(index)}/>
+                        {checkbox.content}
+                    </div>
+                )
+            })}
+            <button onClick={addCheckboxList} >추가하기</button>
         </div>
     );
 };
