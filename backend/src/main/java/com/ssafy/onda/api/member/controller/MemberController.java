20a9152466213c466a200cf322a051070e124baa
김은선 <kus0412@naver.com>
2022-04-27 11:33:18 +0900

    #S06P31A107-95 Feat: 회원정보 조회 API 개발

--- backend/src/main/java/com/ssafy/onda/api/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/onda/api/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.ssafy.onda.api.member.dto.MemberDto;
 import com.ssafy.onda.api.member.dto.request.ReqLoginMemberDto;
 import com.ssafy.onda.api.member.dto.request.ReqMemberDto;
 import com.ssafy.onda.api.member.service.MemberService;
+import com.ssafy.onda.global.common.auth.CustomUserDetails;
 import com.ssafy.onda.global.common.dto.BaseResponseDto;
 import com.ssafy.onda.global.common.util.JwtTokenUtil;
 import com.ssafy.onda.global.common.util.LogUtil;
@@ -11,6 +12,7 @@ import com.ssafy.onda.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
@@ -133,6 +135,29 @@ public class MemberController {
         return BaseResponseDto.builder()
                 .status(status)
                 .msg(msg)
+                .data(data)
+                .build();
+    }
+
+    @GetMapping("/mypage")
+    public BaseResponseDto mypage(Authentication authentication) {
+        log.info("Called API: {}", LogUtil.getClassAndMethodName());
+
+        if (authentication == null) {
+            throw new CustomException(LogUtil.getElement(), UNAUTHORIZED_ACCESS);
+        }
+
+        CustomUserDetails details = (CustomUserDetails) authentication.getDetails();
+        MemberDto memberDto = details.getMemberDto();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("memberId", memberDto.getMemberId());
+        data.put("email", memberDto.getEmail());
+        data.put("nickname", memberDto.getNickname());
+
+        return BaseResponseDto.builder()
+                .status(OK.value())
+                .msg("회원정보 조회에 성공했습니다.")
                 .data(data)
                 .build();
     }
