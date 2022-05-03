d0f88275bea32516acc21cfb60ad1a517bfa69ba
ahnjiae <asj8839@naver.com>
2022-05-03 16:04:59 +0900

    #S06P31A107-102 Refactor: VAC Pattern 으로 수정. logic은 page, component는 view로 분리

--- frontend/pages/api/authApi.ts
+++ frontend/pages/api/authApi.ts
@@ -3,7 +3,7 @@ import {instance} from './axios';
 const COMMON = '/members';
 
 // 회원가입
-export const signup = async (memberData) => {
+export const onSignup = async (memberData) => {
 
   const response = await instance.post(COMMON, memberData);
   return response.data;
