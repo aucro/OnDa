d79776546fc82c5445a988117f01280a22754718
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-05-13 15:55:50 +0900

    #S06P31A107-134 Feat: Cookie 세팅

--- frontend/component/collection/collectionPannel.tsx
+++ frontend/component/collection/collectionPannel.tsx
@@ -9,17 +9,17 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getCollectionAction,getCollectionMemoAction } from 'core/store/actions/collection'
 import { AppDispatch } from 'core/store'
 import { useRouter } from 'next/router'
-const token =
-'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MDEiLCJpc3MiOiJvbmRhLnNzYWZ5LmNvbSIsImV4cCI6MTY1MzM1Nzk4NywiaWF0IjoxNjUyMDYxOTg3fQ._yDfuQ4lL5tbYci6CFY-x08muvg71L5wo1uTH6FMMls_2IVep7jGlh5BMVWtqPXYoLp5Zm6UbzRY1aJYagiLrg'
+import SsrCookie from "ssr-cookie";
+
+// const token =
+// 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MDEiLCJpc3MiOiJvbmRhLnNzYWZ5LmNvbSIsImV4cCI6MTY1MzM1Nzk4NywiaWF0IjoxNjUyMDYxOTg3fQ._yDfuQ4lL5tbYci6CFY-x08muvg71L5wo1uTH6FMMls_2IVep7jGlh5BMVWtqPXYoLp5Zm6UbzRY1aJYagiLrg'
 
 
 const CollectionPannel = ({ onCloseBtn, info }) => {
+  const cookie = new SsrCookie();
+  const token= cookie.get('member');
   const router = useRouter();
-  console.log(info)
-  console.log(info.memoSeqList.toString())
   const previewInfo = useSelector(({ collection }) => collection)
-  console.log(previewInfo.collectionMemoInfo.memoList);
-  
   const appDispatch:AppDispatch = useDispatch();
 
   const getPreviewInfo = () =>{
@@ -36,7 +36,6 @@ const CollectionPannel = ({ onCloseBtn, info }) => {
   }
   useEffect(()=>{
     getPreviewInfo();
-    console.log(previewInfo);
   },[])
   return (
     <div className={styles.pannel}>
