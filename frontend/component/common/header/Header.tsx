9cb5afa89b5e43a373103a913e5f59c69da9842f
unain-dev <daeun84366@gmail.com>
2022-05-16 12:47:56 +0900

    #S06P31A107-87 Fix: 페이지이동 시 새로고침 문제 강제 리로드

--- frontend/component/common/header/Header.tsx
+++ frontend/component/common/header/Header.tsx
@@ -1,21 +1,25 @@
 import Link from 'next/link'
 import styles from 'component/common/header/Header.module.scss'
+import { Router } from 'next/router'
 
 const menus = [
   { name: '월별 모아보기', url: '/collection/month' },
-  { name: '오늘의 다이어리', url: '/diary/' },
+  { name: '오늘의 다이어리', url: '/diary/2020-05-01' },
 ]
 
 const loginMenu = { name: '로그인', url: '/user/login' }
-const logout = { name: '로그아웃', url: '/user/login' }
+const logout = { name: '로그아웃', url: '/user/logout' }
 
 const Header = () => {
   return (
     <div className={styles.naviWrapper}>
       <div className={styles.menuContainer}>
         {menus.map((m) => (
-          <div className={styles.menu}>
-            <Link href={m.url}>{m.name}</Link>
+          <div
+            className={styles.menu}
+            onClick={() => (document.location.href = m.url)}
+          >
+            {m.name}
           </div>
         ))}
       </div>
