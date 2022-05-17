ef0746a23141db9e3b7ac5774480821892e6ad13
unain-dev <daeun84366@gmail.com>
2022-05-17 13:52:26 +0900

    #S06P31A107-157 Design: 메인 골격 디자인

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,29 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from 'styles/scss/Main.module.scss'
+import Login from './user/login'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <h1>Home</h1>
+    <div className={styles.container}>
+      <div className={styles.sideContainer}>
+        <div>
+          <h2>
+            오늘의 기록, 온 : 다
+            <br />
+            온라인 다이어리 온 : 다 를 통해 오늘의 기록을 남기세요
+          </h2>
+          <br />
+          <h3>
+            내가 썼던 기록이 언제였는지 기억이 안날 때
+            <br />
+            여러 기록을 모아 한 눈에 보고싶을 때
+          </h3>
+        </div>
       </div>
-    </>
+      <div className={styles.sideContainer}>
+        <Login />
+      </div>
+    </div>
   )
 }
 
